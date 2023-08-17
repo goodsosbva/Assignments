05d373942ed5f066480f2cb85fd63fa0101375ac
kwon <goodsosbva@naver.com>
2023-08-17 21:49:57 +0900

    Vue side porject start

---
+++ sideProject/vuesideproject/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import CheckboxItem from "./components/CheckboxItem.vue";
+export default {
+  name: "App",
+  components: { CheckboxItem },
+};
+</script>
+
+<template>
+  <div>
+    <h2>App 컴포넌트</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
