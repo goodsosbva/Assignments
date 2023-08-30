d84388d876b63dc4698f02a6cea398a97339005b
kwon <goodsosbva@naver.com>
2023-08-30 20:01:02 +0900

    vue3 setup comAPI Todolist pj 완

---
+++ sideProject/todo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header>
+      <hgroup class="my-5">
+        <h1>나의 할일</h1>
+        <em>{{ today }}</em>
+      </hgroup>
+    </header>
+    <todo-list-container />
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+import TodoListContainer from "./compositions/TodoListContainer.vue";
+
+export default {
+  name: "App",
+  setup() {
+    const today = inject("today");
+    return { today };
+  },
+  components: {
+    TodoListContainer,
+  },
+};
+</script>
+
+<style scope>
+hgroup {
+  text-align: center;
+  font-family: "Arial Bold";
+}
+
+hgroup h1 {
+  font-weight: bolder;
+}
+</style>
