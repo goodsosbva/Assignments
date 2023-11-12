e0dc3507b336457cbce58bf922ca1ac0a66198ae
khs <hs.kwan@okestro.com>
2023-11-12 16:51:30 +0900

    나만의 독후감 프로젝 시작

---
+++ myBookReport/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+  <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+  
+}
+</style>
---
+++ myBookReport/src/layout/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <section id="wrap">
+            <section>상단 영역</section>
+            <section id="container">
+                <router-view />
+            </section>
+            <section>하단 영역</section>
+        </section>
+    </div>
+</template>
