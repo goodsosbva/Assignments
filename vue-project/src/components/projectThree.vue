27aec52bdf3a9d5be9a9edafaa9aaa3bae735d5c
khs <hs.kwan@okestro.com>
2023-11-21 00:21:18 +0900

    pj3: B-3 레이아웃 구현 완료

--- vue-project/src/components/projectThree.vue
+++ vue-project/src/components/projectThree.vue
@@ -1,10 +1,83 @@
-
 <template>
-
+    <body>
+        <header>header</header>
+        <main>
+            <div class="imgSlide">이미지</div>
+            <div class="contents">
+                <div class="notice">공지사항</div>
+                <div class="gallery">갤러리</div>
+                <div class="banner">배너</div>
+            </div>                
+        </main>
+        <footer>
+            푸터
+        </footer>
+    </body>
 </template>
 
 <script setup lang='ts'>
 </script>
 
-<style scoped lang='scss'>
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        list-style:none;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: #ffffff;
+        color:#888888;
+    }
+
+    a {
+        text-decoration:none;
+        display:block;
+    }
+
+    header {
+        width:100%;
+        height:100px;
+        background: #4A4948;
+    }
+
+    main {
+        width: 1200px;
+        margin: 0 auto;
+    }
+
+    .imgSlide {
+        widows: 1200px;
+        height: 300px;
+        background: #888888;
+    }
+
+    .contents {
+        display: flex;
+        justify-content: space-around;
+        height: 200px;
+    }
+
+    .notice {
+        width: 33%;
+        background: #222;
+    }
+
+    .gallery {
+        width: 33%;
+        background: #444
+    }
+
+    .banner {
+        width: 33%;
+        background: #555;
+    }
+
+    footer {
+        width:100%;
+        height:100px;
+        background: #777;
+    }
+
 </style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HomeProject from '../components/projectTwo.vue'
+import HomeProject from '../components/projectThree.vue'
 </script>
 
 <template>
