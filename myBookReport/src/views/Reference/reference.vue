10994bf2c1108504a521321808998235dcbc4f4b
khs <hs.kwan@okestro.com>
2023-12-17 17:47:18 +0900

    refatoring: reference.vue 컴포넌트화 완료 -> listComm.vue

--- myBookReport/src/views/Reference/reference.vue
+++ myBookReport/src/views/Reference/reference.vue
@@ -2,32 +2,7 @@
   <Subpage :pagetit="'자료실'" class="subPageMain">
     <div class="referencepage">
       <div class="subpageguide">도서와 관련된 추가 자료를 다운로드할 수 있습니다.</div>
-      <div class="listwrap">
-        <div class="listnum">총 <strong v-html="lists.length"></strong>건이 등록되어 있습니다.</div>
-      </div>
-      <div class="listbox">
-        <div class="listitem" v-for="(item, index) in lists" :key="index">
-          <div class="listtnum"><img :src="item.img" /></div>
-          <div class="listdetail">
-            <strong v-html="item.title"></strong>
-            <div class="listdate">등록일 : {{ item.date }}</div>
-            <div class="listdec" v-html="item.dec"></div>
-            <div class="itembtns">
-              <button type="button" class="btn btn-outline-primary" :href="item.link1">
-                <i class="bi bi-folder-symlink"></i>소스 파일 내려받기
-              </button>
-              <button type="button" class="btn btn-outline-primary" :href="item.link2">
-                <i class="bi bi-folder-table"></i>정오표
-              </button>
-            </div>
-          </div>
-          <div class="downguide">
-            * 다운로드가 안 될 경우 <br />
-            회사에서 접속한 경우 사내 보안 등의 이유로 다운로드가 되지 않을 수 있습니다. <br />
-            고객센터로 연락주시면 별도로 송부하겠습니다.
-          </div>
-        </div>
-      </div>
+      <listComm :lists="lists"></listComm>
     </div>
   </Subpage>
 </template>
@@ -35,6 +10,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Subpage from '@/layout/components/Subpage.vue'
+import listComm from '@/layout/components/listComm.vue'
 
 interface BookDetail {
   img: string
