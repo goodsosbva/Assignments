c7029ec6a9045895fb4c5bfd90bc8292e09240d3
khs <hs.kwan@okestro.com>
2024-01-27 18:24:51 +0900

    [feat]: WebScoket from ws를 이용하여 간단한 소켓 채팅 구현

--- koom/src/public/js/app.js
+++ koom/src/public/js/app.js
@@ -1,24 +1,44 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+// ws:// : 비암호화 wss:// 암호화 소켓 생성
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    console.log("chk >>> ",  type, payload)
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
 })
 
 socket.addEventListener("message", (message) => {
-    console.log('Just got this:', message.data, "from the server");
+    const li = document.createElement("li");
+    console.log("Type of message.data:", typeof message.data);
+    console.log(message.data);
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from the Server");
 })
 
-function handleSubmit(event) {
+function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
     input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
