e8417bb60cd7b7b138ff440bef45bd5551c1f72a
khs <hs.kwan@okestro.com>
2024-01-28 22:01:46 +0900

    [feat]: 현재 화설시킬 씬 결정 구현

--- apple-page-clone-khs/js/blank.js
+++ apple-page-clone-khs/js/blank.js
@@ -1,6 +1,11 @@
 (() => {
 
     let yOffset = 0
+    // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    // => 현재 위치를 기준으로 위쪽의 스크롤 값
+    let prevScrollHeight = 0;
+    // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+    let currentScene = 0;
     
     const sceneInfo = [
         {
@@ -47,12 +52,22 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
-
-        console.log(sceneInfo)
     }
 
     function scrollLoop() {
-        console.log(yOffset);
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+
+        if (yOffset < prevScrollHeight) {
+            currentScene--;
+        }
+        console.log(currentScene);
     }
 
     window.addEventListener('resize', setLayout)
