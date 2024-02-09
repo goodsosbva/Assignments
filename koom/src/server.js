2c453825fe61e4059b19e4b5d54f005b9da48dc2
kwon <goodsosbva@naver.com>
2024-02-09 23:32:21 +0900

    [feat] WebRTC를 통해 offer 보내기

--- koom/src/server.js
+++ koom/src/server.js
@@ -2,12 +2,12 @@ import http from "http";
 import SocketIO from "socket.io";
 // import {Server} from "socket.io";
 // import {instrument} from "@socket.io/admin-ui";
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.set('view engine', "pug")
-app.set("views", __dirname + '/views');
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
@@ -16,6 +16,17 @@ app.get("/*", (_, res) => res.redirect("/"));
 // app.listen(3000, handleListen);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+});
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
