b6c7c8c24d6d2addf8a1fc15250db95deb5ee8de
khs <hs.kwan@okestro.com>
2024-02-12 22:12:49 +0900

    [feat]: 첫번째 scene에서 분할된 영역에 맞게 sticky하게 애니매션되게 구현 완료 - 1/4

--- apple-page-clone-khs/js/blank.js
+++ apple-page-clone-khs/js/blank.js
@@ -6,6 +6,8 @@
     let prevScrollHeight = 0;
     // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
     let currentScene = 0;
+    // 새로운 scene이 시작되는 순간 true
+    let enterNewScene = false;
     
     const sceneInfo = [
         {
@@ -14,7 +16,20 @@
             heightNum: 5,
             scrollHeight: 0,
             objs: {
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+                messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+                messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+            },
+            values: {
+                messageA_opacity_in: [0, 1, {start: 0.1, end: 0.2}],
+                // messageB_opacity_in: [0, 1, {start: 0.3, end: 0.4}],
+                messageA_translateY_in: [20, 0, {start: 0.1, end: 0.2}],
+
+                messageA_opacity_out: [1, 0, {start: 0.25, end: 0.3}],
+                messageA_translateY_out: [0, -20, {start: 0.25, end: 0.3}],
+
             }
         },
         {
@@ -64,27 +79,95 @@
             }
         }
         document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
 
+    function calcValues(values, currentYOffset) {
+        let rv;
+        // 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+
+        if (values.length === 3) {
+            // start ~ end 사이에 애니메이션
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHegiht = partScrollEnd - partScrollStart;
+
+            if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+                rv = (currentYOffset - partScrollStart) / partScrollHegiht * (values[1] - values[0]) + values[0];
+            } else if (currentYOffset < partScrollStart) {
+                rv = values[0];
+            } else if (currentYOffset > partScrollEnd) {
+                rv = values[1];
+            }
+
+        } else {
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
+        return rv;
+    }
+
+    function playAnimation() {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset -prevScrollHeight;
+        // 현재 scene의 scrollHeight
+        const scrollHegiht = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHegiht;
+
+        switch (currentScene) {
+            case 0:
+                // console.log("0");
+                const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
+                const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
+                const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
+                const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
+
+                if (scrollRatio <= 0.22) {
+                    // in
+                    console.log(messageA_translateY_in);
+                    objs.messageA.style.opacity = messageA_opacity_in;
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+                } else {
+                    // out
+                    objs.messageA.style.opacity = messageA_opacity_out;
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+                }
+                break;
+            case 1:
+                console.log("1");
+                break;
+            case 2:
+                console.log("2");
+                break;
+            case 3:
+                console.log("3");
+                break;
+        }
     }
 
     function scrollLoop() {
+        enterNewScene = false;
         prevScrollHeight = 0;
         for (let i = 0; i < currentScene; i++) {
             prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
         }
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
+            enterNewScene = true;
             if (currentScene === 0) return;
             currentScene--;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
-
+        if (enterNewScene) return;
+        playAnimation();
     }
 
     window.addEventListener('resize', setLayout)
